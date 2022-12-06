cb08999d0be183947047674914ca95fbb2a2665e
Anton Bredenbeck <a.bredenbeck@tudelft.nl>
2022-12-06 18:07:13 +0000

    Extend ros node to include all encoders

--- src/encoders/include/encoders/encoder_node.hpp
+++ src/encoders/include/encoders/encoder_node.hpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <vector>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "cui_devices_amt10x.hpp"
@@ -8,15 +9,51 @@ using namespace std::chrono_literals;
 class EncoderNode : public rclcpp::Node
 {
   public:
-    EncoderNode(uint16_t ppr, uint8_t encoderA, uint8_t encoderB, uint8_t encoderX)
+    EncoderNode()
     : Node("encoder_node"),
-     _NAMES{"base_joint_pitch","base_joint_roll", "middle_joint", "top_joint"},
-     _encoder(ppr, encoderA, encoderB, encoderX)
+     _NAMES{"base_joint_pitch","base_joint_roll", "middle_joint", "top_joint"}
     {
-      this->_publisher = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
-      this->_timer = this->create_wall_timer(50ms, std::bind(&EncoderNode::timer_callback, this));
 
-      this->_encoder.begin();
+      /* Declare all the parameters */
+
+      // PPR for all encoders
+      this->declare_parameter("ppr", 2048);
+      // Pin idx for the encoders                        Signal: A   B   X
+      this->declare_parameter("encoder0_pins", std::vector<int>{ 0,  2,  3});
+      this->declare_parameter("encoder1_pins", std::vector<int>{ 1,  4,  5});
+      this->declare_parameter("encoder2_pins", std::vector<int>{21, 22, 23});
+      this->declare_parameter("encoder3_pins", std::vector<int>{26, 27, 25});
+
+      // Frequency in which to publish the current joint state
+      this->declare_parameter("state_pub_fr", 200);
+
+      /* Read out all the parameters */
+      uint16_t ppr = this->get_parameter("ppr").as_int();
+
+      // Store pin numbers in 2d array
+      std::vector<std::vector<long int>> pins = {this->get_parameter("encoder0_pins").as_integer_array(),
+                                                 this->get_parameter("encoder1_pins").as_integer_array(),
+                                                 this->get_parameter("encoder2_pins").as_integer_array(),
+                                                 this->get_parameter("encoder3_pins").as_integer_array()};
+
+      uint16_t f = this->get_parameter("state_pub_fr").as_int();
+
+      /* Init all the class members */
+      this->_publisher = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+      this->_timer = this->create_wall_timer(std::chrono::milliseconds(int(10e3 * 1.0 / f)), std::bind(&EncoderNode::timer_callback, this));
+
+      for(uint8_t i = 0; i < pins.size(); i++)
+      {
+        /* Init Encoder Driver instances */
+        this->_encoders[i] = new Cui_Devices_Amt10x(i, ppr, pins[i][0], pins[i][1], pins[i][2]);
+      }
+
+      /* Start the encoders */
+      for(uint8_t i = 0; i < pins.size(); i++)
+      {
+        /* Init Encoder Driver instances */
+        this->_encoders[i]->begin();
+      }
     }
 
   private:
@@ -26,6 +63,6 @@ class EncoderNode : public rclcpp::Node
     rclcpp::TimerBase::SharedPtr _timer;
     rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr _publisher;
 
-    Cui_Devices_Amt10x _encoder;
+    Cui_Devices_Amt10x* _encoders[4];
 
 };
--- src/encoders/src/encoder_node.cpp
+++ src/encoders/src/encoder_node.cpp
@@ -5,19 +5,19 @@ void EncoderNode::timer_callback()
     auto message = sensor_msgs::msg::JointState();
     message.header.stamp = this->now();
     message.header.frame_id = this->get_name();
-    message.position = {this->_encoder.getPosition(), 0, 0, 0};
-    for(int i = 0; i < 4 ;i++) message.name.push_back(this->_NAMES[i]);
+    for(int i = 0; i < 4 ;i++)
+    {
+        message.position.push_back(i == 3 ? -this->_encoders[i]->getPosition() : this->_encoders[i]->getPosition());
+        message.name.push_back(this->_NAMES[i]);
+    }
 
     this->_publisher->publish(message);
 }
 
 int main(int argc, char * argv[])
 {
-    uint16_t ppr = 2048;
-    uint8_t encoderA = 2, encoderB = 3, encoderX = 0;
-
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<EncoderNode>(ppr, encoderA, encoderB, encoderX));
+    rclcpp::spin(std::make_shared<EncoderNode>());
     rclcpp::shutdown();
     return 0;
 }
