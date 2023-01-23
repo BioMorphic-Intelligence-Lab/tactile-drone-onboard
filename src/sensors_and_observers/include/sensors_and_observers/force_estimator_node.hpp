d17e1440b38e33124dc0d37d04b9ada8ef8794ae
Anton <a.bredenbeck@tudelft.nl>
2023-01-23 13:15:01 +0100

    Add force estimator node stump

--- src/sensors_and_observers/include/sensors_and_observers/force_estimator_node.hpp
+++ src/sensors_and_observers/include/sensors_and_observers/force_estimator_node.hpp
@@ -2,20 +2,19 @@
 #include <vector>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
+#include "geometry_msgs/msg/wrench_stamped.hpp"
+
 #include "cui_devices_amt10x.hpp"
 
-using namespace std::chrono_literals;
-
-class EncoderNode : public rclcpp::Node
+class ForceEstimatorNode : public rclcpp::Node
 {
 public:
-  EncoderNode()
-      : Node("encoder_node"),
+  ForceEstimatorNode()
+      : Node("force_estimator"),
         _NAMES{"base_joint_pitch", "base_joint_roll", "middle_joint", "top_joint"}
   {
 
     /* Declare all the parameters */
-
     // PPR for all encoders
     this->declare_parameter("ppr", 2048);
     // Pin idx for the encoders                        Signal: A   B   X
@@ -25,7 +24,8 @@ public:
     this->declare_parameter("encoder3_pins", std::vector<int>{26, 27, 25});
 
     // Frequency in which to publish the current joint state
-    this->declare_parameter("state_pub_fr", 24);
+    this->declare_parameter("joint_pub_fr", 24);
+    this->declare_parameter("force_pub_fr", 24);
 
     /* Read out all the parameters */
     uint16_t ppr = this->get_parameter("ppr").as_int();
@@ -40,25 +40,36 @@ public:
     uint8_t B[4] = {uint8_t(pins[0][1]), uint8_t(pins[1][1]), uint8_t(pins[2][1]), uint8_t(pins[3][1])};
     uint8_t X[4] = {uint8_t(pins[0][2]), uint8_t(pins[1][2]), uint8_t(pins[2][2]), uint8_t(pins[3][2])};
 
-    uint16_t f = this->get_parameter("state_pub_fr").as_int();
+    uint16_t f_joint = this->get_parameter("joint_pub_fr").as_int();
+    uint16_t f_force = this->get_parameter("force_pub_fr").as_int();
 
     /* Init all the class members */
-    this->_publisher = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
-    this->_timer = this->create_wall_timer(std::chrono::milliseconds(int(1e3 * 1.0 / f)), std::bind(&EncoderNode::timer_callback, this));
+    this->_joint_publisher = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+    this->_force_publisher = this->create_publisher<geometry_msgs::msg::WrenchStamped>("force", 10);
+
+    this->_joint_timer = this->create_wall_timer(std::chrono::milliseconds(int(1e3 * 1.0 / f_joint)), std::bind(&ForceEstimatorNode::joint_timer_callback, this));
+    this->_f_timer = this->create_wall_timer(std::chrono::milliseconds(int(1e3 * 1.0 / f_force)), std::bind(&ForceEstimatorNode::force_timer_callback, this));
 
     /* Init Encoder Driver instances */
     this->_encoders = new Cui_Devices_Amt10x(4, ppr, A, B, X);
 
     /* Start the encoders */
     this->_encoders->begin();
+
+    /* Start the force estimator */
+    // TODO
   }
 
 private:
   const std::string _NAMES[4];
-  void timer_callback();
+  void joint_timer_callback();
+  void force_timer_callback();
 
-  rclcpp::TimerBase::SharedPtr _timer;
-  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr _publisher;
+  rclcpp::TimerBase::SharedPtr _joint_timer;
+  rclcpp::TimerBase::SharedPtr _f_timer;
+
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr _joint_publisher;
+  rclcpp::Publisher<geometry_msgs::msg::WrenchStamped>::SharedPtr _force_publisher;
 
   Cui_Devices_Amt10x *_encoders;
 };
---
+++ src/sensors_and_observers/src/force_estimator_node.cpp
@@ -0,0 +1,45 @@
+#include "force_estimator_node.hpp"
+
+void ForceEstimatorNode::joint_timer_callback()
+{
+    auto message = sensor_msgs::msg::JointState();
+    message.header.stamp = this->now();
+    message.header.frame_id = this->get_name();
+    
+    float pos[4] = {}, vel[4] = {0.0,0.0,0.0,0.0};
+    this->_encoders->getPosition(pos);
+    this->_encoders->getVelocity(vel);
+
+    for(int i = 0; i < 4; i++)
+    {
+        message.position.push_back(i == 3 ? -pos[i] : pos[i]);
+        message.velocity.push_back(vel[i]);
+        message.name.push_back(this->_NAMES[i]);
+    }
+
+    this->_joint_publisher->publish(message);
+}
+
+void ForceEstimatorNode::force_timer_callback()
+{
+    auto message = geometry_msgs::msg::WrenchStamped();
+    message.header.stamp = this->now();
+    message.header.frame_id = this->get_name();
+
+    message.wrench.force.x = 0.0;
+    message.wrench.force.y = 0.0;
+    message.wrench.force.z = 0.0;
+    
+    message.wrench.torque.x = 0.0;
+    message.wrench.torque.y = 0.0;
+    message.wrench.torque.z = 0.0;
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    
+    rclcpp::spin(std::make_shared<ForceEstimatorNode>());
+    rclcpp::shutdown();
+    return 0;
+}
